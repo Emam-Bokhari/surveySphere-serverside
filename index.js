c356cf623d54af0058c025cb7818202d8fed1730
Moshfiqur Rahman <moshfiqurrahman37@gmail.com>
2023-11-26 19:01:16 +0600

    implement Express server with MongoDB integration - Configure middleware for Cross-Origin Resource Sharing (CORS), JSON parsing, and cookie handling.  Retrieve and send all survey data from the survey collection. Insert a new survey into the survey collection. Retrieve details of a specific survey using its ID from the survey collection. Insert a new like for a survey into the like collection. Retrieve and send all comments or a specific comment using the commentId query parameter from the comment collection. Insert a new comment into the comment collection.

---
+++ index.js
@@ -0,0 +1,117 @@
+const express = require("express")
+const cors = require("cors")
+const cookieParser = require("cookie-parser")
+require("dotenv").config()
+const port = process.env.PORT || 3000
+const app = express()
+
+// middleware
+app.use(cors())
+app.use(express.json())
+app.use(cookieParser())
+
+// surveySphere
+// q8ZbhVnqLWqhm2QM
+
+
+
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const uri = `mongodb+srv://${process.env.USERS_DB}:${process.env.DB_PASS}@cluster0.kndeci6.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    client.connect();
+
+    //  database collection
+    const database = client.db("surveySphereDB")
+    const surveyCollection = database.collection("survey")
+    const likeCollection = database.collection("like")
+    const commentCollection = database.collection("comment")
+
+    // get :: show survey data
+    app.get("/api/v1/show-servey", async (req, res) => {
+      const result = await surveyCollection.find().toArray()
+      res.send(result)
+    })
+
+    // post :: create survey
+    app.post("/api/v1/create-survey", async (req, res) => {
+      const survey = req.body
+      console.log(survey);
+      const result = await surveyCollection.insertOne(survey)
+      res.send(result)
+    })
+
+
+    // get :: survey details
+    app.get("/api/v1/:surveyId/survey-details", async (req, res) => {
+      const surveyId = req.params.surveyId
+      const query = { _id: new ObjectId(surveyId) }
+      const result = await surveyCollection.findOne(query)
+      res.send(result)
+    })
+
+
+    // post :: survey like
+    app.post("/api/v1/like-survey", async (req, res) => {
+      const surveyLike = req.body
+      console.log(surveyLike);
+      const result = await likeCollection.insertOne(surveyLike)
+      res.send(result)
+    })
+
+
+    // get :: show comment
+    app.get("/api/v1/show-comment",async(req,res)=>{
+      let query={}
+      if(req.query.commentId){
+        query={commentId:req.query.commentId}
+      }
+      const result=await commentCollection.find(query).toArray()
+      res.send(result)
+    })
+
+
+    // post :: create comment 
+    app.post("/api/v1/comment", async (req, res) => {
+      const comment = req.body
+      console.log(comment);
+      const result = await commentCollection.insertOne(comment)
+      res.send(result)
+    })
+
+
+
+
+
+
+
+
+    // Send a ping to confirm a successful connection
+    client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+app.get("/health", (req, res) => {
+  res.send('Server is running...');
+})
+
+app.listen(port, (req, res) => {
+  console.log(`port${port}`);
+})
